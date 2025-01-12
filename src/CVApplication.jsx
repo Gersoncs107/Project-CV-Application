b132e9ccf1cbc37fb6f7564e858d16fed86db03f
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-12 20:10:02 -0300

    react

--- src/CVApplication.jsx
+++ src/CVApplication.jsx
@@ -6,9 +6,18 @@ export default function Form(){
         <div>
             <h1>Resume</h1>
             <form>
-                <h2>General Information</h2>
-                <label htmlFor=""></label>
-                <input type="text" id="name" name="name" placeholder="Digite seu nome"/>
+                <div>
+                    <section>
+                        <h2>General Information</h2>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Digite seu nome"/>
+
+                        <label htmlFor="email">Email</label>
+                        <input type="email"  id="email" name="email" placeholder="example@.com"/>
+                    </section>
+                   
+                </div>
+                
             </form>
         </div>
 
