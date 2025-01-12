fb9d0f01c5df632cddb6c2fa3a3db9f872f54698
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-12 20:00:17 -0300

    react

---
+++ src/CVApplication.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Form(){
+    
+    return(
+        <div>
+            <h1>Resume</h1>
+            <div>
+                <h2>General Information</h2>
+                <input type="text" id="name" name="name" placeholder="Digite seu nome"/>
+            </div>
+        </div>
+
+    )
+}
