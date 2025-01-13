0132a20ff17bbea2c07053ba8da388e61e18a2d2
Gersoncs107 <gerson.silva107@gmail.com>
2025-01-12 21:53:13 -0300

    react

--- src/CVApplication.jsx
+++ src/CVApplication.jsx
@@ -16,13 +16,22 @@ export default function Form(){
                         <input type="email"  id="email" name="email" placeholder="example@.com"/>
                         
                         <label htmlFor="phone">Phone Number</label>
-                        <input type="text" id="phone" name="phone number"  placeholder="12345"/>
+                        <input type="tel" id="phone" name="phone number"  placeholder="12345"/>
                     </div>
 
                     <div>
                         <h2> Educational experience</h2>
                         <label htmlFor="educational">School Name</label>
                         <input type="text" id="educational" name="educational" />
+
+                        <label htmlFor="title">Title of Study</label>
+                        <input type="text" id="title" name="title of study" />
+
+                        <label htmlFor="date">Date of study</label>
+                        <input type="date" id="date" name="date of study"/>
+                        
+
+
                     </div>
                    
                 </div>
